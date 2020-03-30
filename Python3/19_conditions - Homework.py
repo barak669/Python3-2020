311f29efb9ba479c281b6f48dfa00c307e0f236d
barak669 <57372287+barak669@users.noreply.github.com>
2020-03-30 21:08:42 +0300

    Add files via upload

---
+++ Python3/19_conditions - Homework.py
@@ -0,0 +1,18 @@
+num=int(input("enter number:"))
+arr=["zero","one","two","three","four","five","six","seven","eight","nine"]
+
+if num< 10:
+    print ("output:",arr[num])
+
+elif num>=10 and num<=99:
+	sum=(num%10+int (num/10))
+	print(num%10+int (num/10))
+	print("output:" ,num ,"is 2 digit number sum is",sum)
+
+elif num>=100 and num<=999:
+	mul=((num%10)*(int(num%100/10))*(int(num/100)) ) 
+	print((num%10)*(int(num%100/10))*(int(num/100)) )
+	print("output:" ,num ,"is 3 digit number mul is",mul)
+
+else:
+	print("output: number has more than 3 digits")
