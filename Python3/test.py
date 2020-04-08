9c8be202cb995f82bf4873b46786c134b3d12ae8
barak669 <barakk9471@gmail.com>
2020-04-08 15:24:53 +0300

    commit

--- Python3/test.py
+++ Python3/test.py
@@ -1,8 +1,15 @@
-num=int(input("Insert a number: "))
-mul=1
+current=int(input("Insert a number: "))
+counter=0
+is_legal=0
 
-while num!=0:
-    mul*=num%10
-    num//=10
+while counter<3:
+    prev=current
+    current=int(input("Insert a number: "))
+    if prev<=current:
+        is_legal+=1
+    counter+=1
 
-print(mul)
+if(is_legal==3):
+    print("you entered a valid set")
+else:
+    print("you entered invalid set")  
