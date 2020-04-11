e739772a5ee9a8e2c9775dfd593b53ef25373505
barak669 <barakk9471@gmail.com>
2020-04-11 18:56:58 +0300

    commit

--- Python3/Passover-Homework.py
+++ Python3/Passover-Homework.py
@@ -1,17 +1,13 @@
 ID = (input("Enter your ID: "))
-
-n = [1,2,1,2,1,2,1,2,1]
-  
-  
-odd_i = [] 
-even_i = [] 
-for i in range(0, len(ID)): 
-    if i % 2: 
-        even_i.append(n) 
-    else : 
-        odd_i.append(n) 
-  
-res = odd_i + even_i 
+w="121212121"
 
 print(ID)
-print(res)
+print(int(w))
+
+
+counter=0
+while counter<10:
+    ID*w
+    counter+=1
+    print(ID)
+    print(int(w))
