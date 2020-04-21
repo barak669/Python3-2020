e76e1d54bf22959ecd9671ec16607f93f554278f
Barak <Barakk9471@gmail.com>
2020-04-21 19:37:16 +0300

    commit

---
+++ Python3/PassoverHomework.py
@@ -0,0 +1,14 @@
+  
+ID = (input("Enter your ID: "))
+w="121212121"
+
+print(int(ID))
+print(int(w))
+
+
+counter=0
+while counter<10:
+    ID*w
+    counter+=1
+    print(int(ID))
+    print(int(w))
--- Python3/test.py
+++ Python3/test.py
@@ -1,2 +1,9 @@
-num=13
-print(num%10+int (num/10))
+ID = int (input("Enter your ID: "))
+w=121212121
+
+print(ID)
+print(w)
+
+
+
+print(ID[0])
