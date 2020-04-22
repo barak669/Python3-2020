ca6ce7b962579217f9675779a63f15773de03a1e
Barak <Barakk9471@gmail.com>
2020-04-22 16:02:01 +0300

    commit

--- Python3/PassoverHomework.py
+++ Python3/PassoverHomework.py
@@ -1,11 +1,27 @@
 ID = (input("Enter your ID: "))
 w = [1,2,1,2,1,2,1,2,1]
 counter=0
+sum1=0
 
+print(ID)
+print(w)
 
 
 while counter<len(ID):
-    sum = int(ID[1]) * (w[counter])
-    print(sum)
+    sum = int(ID[counter]) * (w[counter])
+    print(sum, end = ' ')
+    
     counter+=1
+   
+
+       
+print()
+counter=0
+while counter<len(ID):
+    sum = int(ID[counter]) * (w[counter])
+    counter+=1
+
+    if sum>9:
+       sum = (sum%10) + (sum//10)
+       print(sum, end = ' ')
 
