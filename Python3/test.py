acd49f53d13a3d8efd342379d6d927d42d97c3f9
Barak <Barakk9471@gmail.com>
2020-04-22 12:29:59 +0300

    commit

--- Python3/test.py
+++ Python3/test.py
@@ -2,8 +2,17 @@ ID = (input("Enter your ID: "))
 w = [1,2,1,2,1,2,1,2,1]
 counter=0
 
+print(ID)
+print(w)
+
 
 while counter<len(ID):
-    sum = int(ID[counter]) * (w[counter]).end
-    print(sum)
+    sum = int(ID[counter]) * (w[counter])
+    print(sum, end = ' ')
     counter+=1
+    
+    if sum>9:
+       sum = (sum%10) + (sum//10)
+       print()
+       print(sum)
+
