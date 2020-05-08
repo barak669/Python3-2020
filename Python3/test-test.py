3977a697bf75e37c95417093f2f6809371cbb311
barak669 <barakk9471@gmail.com>
2020-05-08 11:23:22 +0300

    commit

--- Python3/test-test.py
+++ Python3/test-test.py
@@ -1,22 +1,50 @@
+#include<iostream>
+using namespace std;
 
-  
-# initializing list 
-n = [1,2,1,2,1,2,1,2,1]
-  
-# printing original list 
-print("The n : " + str(n)) 
-  
-# using naive method 
-# Separating odd and even index elements 
-odd_i = [] 
-even_i = [] 
-for i in range(0, len(ID)): 
-    if i % 2: 
-        even_i.append(n[i]) 
-    else : 
-        odd_i.append(n[i]) 
-  
-res = odd_i + even_i 
-  
-# print result 
-print("Seprated odd and even index list: " + str(res))
+int main()
+{
+    char ch;
+    int num, i, j;
+    
+   cout<<"enter base number\n";
+   cin>>num;
+   cout<<"enter char\n";
+   cin>>ch;
+   
+   if(num%2 == 1)
+   {
+       for (i=0; i<=num/2; i++) //rows
+       {
+           for(j=0; j<num/2-i; j++) //backspace
+           {
+               cout<<" ";
+           }
+           for( j=0; j<2*i+1; j++) //char
+           {
+               cout<<ch;
+           }
+           cout<<"\n";
+       }
+       
+   }
+   else
+   {
+       cout<<"wrong input\n";
+   }
+    return 0;
+}
+
+
+
+
+num = int(input("Enter number: "))
+
+for outer in range(0, num):
+    row=''
+    for inner in range(0, num):
+        if (inner==0 or inner==num-1 or outer==0 or outer==num-1):
+            row+='*'
+        else:
+            row+=' '
+
+    print(row)
--- Python3/test.py
+++ Python3/test.py
@@ -1,41 +1,13 @@
-<<<<<<< HEAD
-num = input("Enter number: ")
-ch = input("Enter char: ")
+num = int(input("Enter number: "))
 
-   
-   if(num%2 == 1)
-   {
-       for (i=0; i<=num/2; i++) //rows
-       {
-           for(j=0; j<num/2-i; j++) //backspace
-           {
-               cout<<" ";
-           }
-           for( j=0; j<2*i+1; j++) //char
-           {
-               cout<<ch;
-           }
-           cout<<"\n";
-       }
-       
-   }
-   else
-   {
-       cout<<"wrong input\n";
-   }
-=======
-num=int(input("Insert a number: "))
-counter=2
+for outer in range(0, num):
+    row=''
+    for inner in range(0, num):
+        if (inner==0 or outer==num-1):
+            row+='*'
+        if (inner==(num-4+inner)):
+            row+=''
+        else:
+            row+='*'
 
-
-while counter<num:
-    if num%counter==0:
-        break
-    counter+=1
-
-
-if counter==num:
-    print("prime number")
-else:
-    print("not a prime number")
->>>>>>> e739772a5ee9a8e2c9775dfd593b53ef25373505
+    print(row)
