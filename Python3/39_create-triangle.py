b6218213cf57129e1d87994bdb03dcb022ee72dc
barak669 <barakk9471@gmail.com>
2020-05-08 14:32:23 +0300

    commit

--- Python3/39_create-triangle.py
+++ Python3/39_create-triangle.py
@@ -2,9 +2,9 @@ num = int(input("Enter number: "))
 
 for i in range(0, num):
     row = ''
-    for j in range(0, num-i-1):
-        row+=''
     for j in range(0, i+1):
         row+='*'
+    for j in range(0, num-i-1):
+        row+=''
 
     print(row)
