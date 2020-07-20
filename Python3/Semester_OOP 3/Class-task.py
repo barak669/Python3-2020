84b748ca2d8026969ad6b3c7a2eaf6c3585ba1fc
Barak <Barakk9471@gmail.com>
2020-07-20 21:17:10 +0300

    commit

--- Python3/Semester_OOP 3/Class-task.py
+++ Python3/Semester_OOP 3/Class-task.py
@@ -1,40 +1,40 @@
 class Student:
     sum_score = 0
     sum_age = 0
-    sum_student = 0
+    sum_student = 0 
 
     # constractor
-    def __init__(self, score, age):
+    def __init__(self, age, score):
         self.score=score
         self.age=age
-        Student.sum_age += age/4
-        Student.sum_score += score/4
+        Student.sum_student += 1
+        Student.sum_age += age
+        Student.sum_score += score
 
     @classmethod
-    def score_average(class_level):
-        print(f"The average of the score is {class_level.sum_score}")
+    def score_average(cls):
+        return cls.sum_score/cls.sum_student
+        
 
     @classmethod
-    def age_average(class_level):
-        print(f"The average of the age is {class_level.sum_age}")
+    def age_average(cls):
+        return cls.sum_age/cls.sum_student
+
+    @classmethod
+    def print_info(self):
+        print(f"age: {self.age}, score: {self.score}")
 
 
 
+student=[
+    Student(10,60),
+    Student(15,70),
+    Student(20,60),
+    Student(25,70),
+]
 
+for student in student:
+    student.print_info()
 
-
-s1=Student(90, 20)
-s1.score_average()
-s1.age_average()
-
-s2=Student(88, 40)
-s2.score_average()
-s2.age_average()
-
-s3=Student(79, 33)
-s3.score_average()
-s3.age_average()
-
-s4=Student(95, 25)
-s4.score_average()
-s4.age_average()
+print("score average: ",Student.sum_score())
+print("age average: ",Student.sum_age())
