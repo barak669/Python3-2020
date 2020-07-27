6658bff7c416823570e61fbfae44ba94e6b369e6
barak669 <barakk9471@gmail.com>
2020-07-27 21:11:08 +0300

    commit

--- Python3/Semester_OOP 3/Class-task2.py
+++ Python3/Semester_OOP 3/Class-task2.py
@@ -1,7 +1,8 @@
 class Cinema:
 
     def __init__(self, name,num_of_chairs,open_hour,close_hour):
-        self.name=nameself.num_of_chairs=num_of_chairs
+        self.name=name
+        self.num_of_chairs=num_of_chairs
         self.open_hour=open_hour
         self.close_hour=close_hour
 
@@ -9,7 +10,7 @@ class Cinema:
         return self.close_hour-self.open_hour
 
     def print_info(self):
-        return f*name:{self.name}, open_hour: {self.}
+        return f*"name:{self.name}, open_hour: {self.open_hour}, close_hour: {self.close_hour}, num_of_chairs: {self.num_of_chairs}"
 
     @property
     def num_of_chairs(self):
@@ -22,12 +23,12 @@ class Cinema:
 
     @property
     def open_hour(self):
-        return self.__open_hur
+        return self.__open_hour
 
 
-    @open_huor.setter
-    def open_huor(self, hour):
-        self.__open_huor = hour if hour>6 and hour<12 else 6
+    @open_hour.setter
+    def open_hour(self, hour):
+        self.__open_hour = hour if hour>6 and hour<12 else 6
 
     @property
     def close_hour(self):
@@ -37,5 +38,11 @@ class Cinema:
     @close_hour.setter
     def close_hour(self, hour):
         self.__close_hour = hour if hour>18 and hour<23 else 6
+
+
+arr= [
+    Cinema("Yes", 38,7,22),
+    Cinema("Hen", -9,5,12)
+]
 
 
