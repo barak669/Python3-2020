e7e5c251c663281120a3af80ea4b06fdda84e39a
Barak <Barakk9471@gmail.com>
2020-08-03 20:45:00 +0300

    commit

---
+++ Python3/Semester_OOP 3/Class-task3_CinemaManager/cinema.py
@@ -0,0 +1,6 @@
+class Cinema:
+    def __init__(self):
+        self.arr_room = []
+        self.arr_movie = []
+        self.max_movie=100
+        self.max_room=6
---
+++ Python3/Semester_OOP 3/Class-task3_CinemaManager/movie.py
@@ -0,0 +1,7 @@
+class Movie:
+    def __init__(self,name, length):
+        self.name=name
+        self.length=length
+    
+    def get_info(self):
+        return "name:"
